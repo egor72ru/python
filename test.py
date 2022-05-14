4d262f22d5e4cf272fa64b444f180b9767c4d741
egor72ru <105519766+egor72ru@users.noreply.github.com>
2022-05-14 15:51:18 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,34 @@
+ name = "вася"
+age = 30
+ print(name)
+print(int(input(age)
+ string1=input("введите имя")
+ #umber1=int(input("введите возраст")
+print(string1,number1) 
+ 
+ 
+time=int(input("введи время в секундах"))
+hours=time //3600
+minuta=(time-hours //3600)
+second=time-(hours*3600+minuta*60)
+print(second,minuta,hours,time)
+
+
+n = int(input("Введите число "))
+a= 
+
+
+
+a=abs(int(input("введите число")))
+b=a //10
+while a>=1
+if a //10>b
+print("молодец")
+else :
+    print("не молодец",b)
+    
+
+
+
+
+      
